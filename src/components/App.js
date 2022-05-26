3ee7a20c946594c802db1863e12bff3f81534645
Steven Lomax <ste.lomax@gmail.com>
2022-05-26 22:45:52 +0100

    location details

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,37 @@
+import PropTypes from "prop-types";
 import React from "react";
 
 import "../styles/App.css";
 
-function App() {
+import LocationDetails from "./LocationDetails";
+
+// function App(props) {
+//   return (
+//     <div className="App">
+//       <h1>Weather App</h1>
+//       <LocationDetails
+//         city={props.location.city}
+//         country={props.location.country}
+//       />
+//     </div>
+//   );
+// }
+
+function App({ location }) {
+  const { city, country } = location;
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      <h1>Weather App by Steven!</h1>
+      <LocationDetails city={city} country={country} />
     </div>
   );
 }
+
+App.propTypes = {
+  location: PropTypes.shape({
+    city: PropTypes.string,
+    country: PropTypes.string,
+  }).isRequired,
+};
 
 export default App;
