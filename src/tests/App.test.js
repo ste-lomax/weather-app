582184a0059f6e8ce9e4eb9953f062f35175bc9c
Steven Lomax <ste.lomax@gmail.com>
2022-06-07 06:51:18 +0100

    location details reformatted and tests passed

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,18 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "../components/App";
+import forecast from "../data/forecast.json";
 
 test("renders Weather App title", () => {
-  render(<App />);
+  render(<App location={forecast.location} />);
   const linkElement = screen.getByText(/Weather App/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+describe("App", () => {
+  test("renders App component correctly", () => {
+    render(<App location={forecast.location} />);
+    const h1Element = screen.getByText(/Manchester, UK/i);
+    expect(h1Element).toBeInTheDocument();
+  });
 });
---
+++ src/tests/components/ForecastSummary.test.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import ForecastSummary from "../../components/ForecastSummary";
+
+describe("ForecastSummary", () => {
+  it("renders the forecast summary for an individual day"),
+    () => {
+      const {} = render();
+    };
+});
